653f17c6a41e06eaac14d2681ea530da855eecda
Oliver <olivergestanley@gmail.com>
2025-03-12 17:35:57 +0000

    add func tests

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,15 @@ int main() {
     G.add_node(2);
     G.add_edge(1, 2, {{"weight", "3.14"}});
     std::cout << G << std::endl;
+    Graph G_DG = G.to_directed();
+    std::cout << G_DG << std::endl;
 
     DiGraph DG;
     DG.add_node(1, {{"color", "red"}});
     DG.add_node(2);
     DG.add_edge(1, 2, {{"weight", "3.14"}});
     std::cout << DG << std::endl;
+    Graph DG_G = DG.to_undirected();
+    std::cout << DG_G << std::endl;
     return 0;
 }
