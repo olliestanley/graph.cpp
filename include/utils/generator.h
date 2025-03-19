dfd51b3b2ff630958e3e2e5c6414bfd48c0f56bf
Oliver <olivergestanley@gmail.com>
2025-03-19 19:59:55 +0000

    more efficient generator

--- include/utils/generator.h
+++ include/utils/generator.h
@@ -5,13 +5,17 @@
 #include <cstdlib>
 #include <iterator>
 
+#include <exception>
+#include <utility>
+#include <type_traits>
+
 template<typename T>
 class Generator {
 public:
     struct promise_type;
     using handle_type = std::coroutine_handle<promise_type>;
 
-    explicit Generator(handle_type h) : coro(h) {}
+    explicit Generator(handle_type h) noexcept : coro(h) {}
     Generator(const Generator&) = delete;
     Generator(Generator&& other) noexcept : coro(other.coro) { other.coro = nullptr; }
     ~Generator() { if (coro) coro.destroy(); }
@@ -34,7 +38,7 @@ public:
         using pointer = const T*;
         using reference = const T&;
 
-        iterator() : coro(nullptr), done(true) {}
+        iterator() noexcept : coro(nullptr), done(true) {}
         explicit iterator(handle_type h) : coro(h) { resume(); }
 
         iterator& operator++() { 
@@ -42,7 +46,8 @@ public:
             return *this;
         }
 
-        T operator*() const { return coro.promise().current_value; }
+        const T& operator*() const { return coro.promise().current_value; }
+        const T* operator->() const { return &(coro.promise().current_value); }
         
         bool operator==(std::default_sentinel_t) const { return done; }
         bool operator!=(std::default_sentinel_t) const { return !done; }
@@ -53,6 +58,11 @@ public:
 
         void resume() {
             coro.resume();
+            if (coro.done()) {
+                if (coro.promise().exception) {
+                    std::rethrow_exception(coro.promise().exception);
+                }
+            }
             done = coro.done();
         }
     };
@@ -62,7 +72,7 @@ public:
             return iterator{};
         return iterator{coro};
     }
-    
+
     std::default_sentinel_t end() { return {}; }
 
 private:
@@ -72,21 +82,23 @@ private:
 template<typename T>
 struct Generator<T>::promise_type {
     T current_value;
+    std::exception_ptr exception;
 
     auto get_return_object() {
         return Generator{handle_type::from_promise(*this)};
     }
-    
-    std::suspend_always initial_suspend() { return {}; }
+
+    std::suspend_always initial_suspend() noexcept { return {}; }
     std::suspend_always final_suspend() noexcept { return {}; }
-    
-    std::suspend_always yield_value(T value) {
-        current_value = value;
+
+    template<typename U>
+    std::suspend_always yield_value(U&& value) noexcept(std::is_nothrow_assignable_v<T&, U&&>) {
+        current_value = std::forward<U>(value);
         return {};
     }
-    
+
     void return_void() {}
-    void unhandled_exception() { std::exit(1); }
+    void unhandled_exception() { exception = std::current_exception(); }
 };
 
 #endif
