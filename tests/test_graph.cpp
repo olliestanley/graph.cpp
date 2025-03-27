ac67956bfc1cde024aab99762c30a0bae547d605
Oliver <olivergestanley@gmail.com>
2025-03-27 17:57:08 +0000

    generator test

--- tests/test_graph.cpp
+++ tests/test_graph.cpp
@@ -49,3 +49,21 @@ TEST(GraphTest, BFSOrder) {
     std::vector<Graph::Node> expectedOrder = {1, 2, 3, 4, 5, 6, 7};
     EXPECT_EQ(order, expectedOrder);
 }
+
+TEST(GraphTest, BFSGenerator) {
+    Graph g;
+    g.add_edge(1, 2);
+    g.add_edge(1, 3);
+    g.add_edge(2, 4);
+    g.add_edge(2, 5);
+    g.add_edge(3, 6);
+    g.add_edge(3, 7);
+    Graph::Node start = 1;
+    Generator<Graph::Node> order = bfs_generator(g, start);
+    std::vector<Graph::Node> expectedOrder = {1, 2, 3, 4, 5, 6, 7};
+    for (Graph::Node node : order) {
+        EXPECT_EQ(node, expectedOrder.front());
+        expectedOrder.erase(expectedOrder.begin());
+    }
+    EXPECT_TRUE(expectedOrder.empty());
+}
